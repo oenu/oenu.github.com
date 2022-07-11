c9cbc9cedbe2019da6fef6fd1c9a6c67c0f07fe0
oenu <51684443+oenu@users.noreply.github.com>
2022-07-11 16:17:02 -0400

    added mantine theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+// Mantine
+import { MantineProvider } from "@mantine/core";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        colorScheme: "dark",
+      }}
+    >
+      <App />
+    </MantineProvider>
   </React.StrictMode>
-)
+);
