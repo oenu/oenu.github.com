b6aedf43e46d167505004dff49b369f13a869d34
oenu <51684443+oenu@users.noreply.github.com>
2022-07-11 14:32:08 -0400

    added markdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,64 @@
-import { useState } from "react";
-import logo from "./logo.svg";
+import { useState, useEffect } from "react";
+import { createClient } from "@supabase/supabase-js";
+
+// Styles
 import "./App.css";
 
+// Components
+import Post from "./Components/Post";
+
+// Types
+import { PostType } from "./types";
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient(
+  "https://fexutrlesdaielcvnpiu.supabase.co",
+  /* cspell: disable-next-line */
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZleHV0cmxlc2RhaWVsY3ZucGl1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTc1NjIxMTAsImV4cCI6MTk3MzEzODExMH0.nnP00yRLtb3tjm3aHLlX4eyBYwGVfaVT9fMSllh17Jk"
+);
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [posts, setPosts] = useState(Array<PostType>());
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    // Get posts from the database
+    const getPosts = async () => {
+      const { data, error } = await supabase.from("posts");
+
+      if (error) {
+        console.error(error);
+      }
+      if (data) {
+        setPosts(data);
+        setLoading(false);
+        console.log(data);
+      }
+
+      return () => {
+        console.log("unmounting");
+      };
+    };
+
+    getPosts();
+  }, []);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <header className="App-header"></header>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        posts.map((post) => (
+          <Post
+            key={post.id}
+            body={post.body}
+            id={post.id}
+            title={post.title}
+            hidden={post.hidden}
+            created_at={post.created_at}
+          />
+        ))
+      )}
     </div>
   );
 }
---
+++ src/types.d.ts
@@ -0,0 +1,7 @@
+export interface PostType {
+  id: string;
+  body: string;
+  title: string;
+  created_at: string;
+  hidden: boolean;
+}
