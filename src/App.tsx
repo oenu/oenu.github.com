11cb7e64c60cc58191861e15c4620a7c77ccfd3e
oenu <51684443+oenu@users.noreply.github.com>
2022-07-11 13:19:21 -0400

    editing

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn Reacter
+            Learn React
           </a>
           {" | "}
           <a
